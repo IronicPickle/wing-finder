8ddcacb2ea0f2df723200d805f0db0bcd6b7800d
BuildTools <unconfigured@null.spigotmc.org>
2019-09-27 18:58:15 +0100

    Reduced help menus based of users permissions

--- src/wf_libs/utils/getCommand.js
+++ src/wf_libs/utils/getCommand.js
@@ -5,6 +5,8 @@ const checkChannelPerm = require("./checkChannelPerm.js");
 // Vars
 const config = require("../../config/global.json");
 const prefix = config.discord.commands.prefix;
+// DB models
+const GuildData = require("../../models/guild_data.js");
 
 function getCommand(input, callback, msg) {
   var isValid = input.toLowerCase().startsWith(prefix);
@@ -23,27 +25,34 @@ function getCommand(input, callback, msg) {
       callback(commandClass, "Invalid usage.");
       return;
     } else if(msg) {
-      checkChannelPerm(msg.guild, msg.channel, (validated, activeChannel, unbound, claimed) => {
-        if(!claimed && command != "claim" && command != "status") {
+      var guild = msg.guild;
+      var channel = msg.channel;
+      var member = msg.member;
+      GuildData.findOne({guildID: guild.id}).exec().then(guildData => {
+        if(!guildData) throw new Error("No data returned!");
+        var validated = checkChannelPerm(guildData, channel);
+        if(!guildData.claimed && command != "claim" && command != "status") {
           callback(null, "This bot has not been claimed, use ' " + prefix + "claim ' to claim it.");
         } else if(!validated && commandClass.RESTRICT_CHANNEL) {
-          if(unbound) {
+          if(!guildData.activeChannel) {
             callback(null, "A command channel has not been bound.\nUse ' " + prefix + "admin bind-channel ' to bind a channel.");
           } else {
-            callback(null, "You can only use commands in <#" + activeChannel + ">.");
+            callback(null, "You can only use commands in <#" + guildData.activeChannel + ">.");
           }
           return;
         } else {
-          checkUserPerm(msg.guild, msg.channel, msg.member, commandClass.REQUIRED_GROUP, validated => {
-            if(!validated) {
-              callback(null, "You do not have permission to use this command.");
-              return;
-            } else {
-              callback(commandClass, null);
-              return;
-            }
-          });
+          var validated = checkUserPerm(guildData, channel, member, commandClass.REQUIRED_GROUP);
+          if(!validated) {
+            callback(null, "You do not have permission to use this command.");
+            return;
+          } else {
+            callback(commandClass, null);
+            return;
+          }
         }
+      }).catch(err => {
+        channel.send("An internal error has occurred.");
+        console.log(err);
       });
     } else {
       callback(commandClass, null);
--- src/wf_libs/utils/printHelp.js
+++ src/wf_libs/utils/printHelp.js
@@ -1,66 +1,81 @@
+// Libs
+const checkUserPerm = require("./checkUserPerm.js");
+const checkChannelPerm = require("./checkChannelPerm.js");
 // DB models
 const RegisteredCommands = require("../../models/registered_commands");
+const GuildData = require("../../models/guild_data.js");
 
 function printPerms(msg, arg) {
+  var guild = msg.guild;
   var channel = msg.channel;
+  var member = msg.member;
   var user = msg.author;
-  if(arg) {
 
-    RegisteredCommands.find({}).then(registered => {
+  RegisteredCommands.find({}).then(registered => {
     if(!registered) throw new Error("No data returned!");
-      var mainStr = "";
-      var registeredCommand = registered.find(obj => obj.name == arg);
-      if(registeredCommand) {
-        var command = registeredCommand.object;
-        mainStr += "__**Command Info**__";
-        mainStr += "\nHelp: " + command.HELP + "\nUsage: " + command.USAGE;
-        if(command.IS_SUB) {
-          var children = command.CHILDREN;
-          mainStr += "\n**__Sub-Commands__**\n";
-          for(var ii in children) {
-            var child = command.COMMAND + " " + children[ii];
-            var subCommand = registered.find(obj => obj.name == child).object;
-            var requiredGroup = subCommand.REQUIRED_GROUP;
-            if(!subCommand.IS_SUB) mainStr += "> **> " + subCommand.USAGE + "** - "
-            if(requiredGroup) mainStr += "(" + (requiredGroup.charAt(0).toUpperCase() + requiredGroup.slice(1)) + ") ";
-            if(!subCommand.IS_SUB) mainStr += "*" + subCommand.HELP + "*\n";
+    GuildData.findOne({guildID: guild.id}).exec().then(guildData => {
+      if(!guildData) throw new Error("No data returned!");
+
+      if(arg) {
+
+        var mainStr = "";
+        var registeredCommand = registered.find(obj => obj.name == arg);
+        if(registeredCommand) {
+          var command = registeredCommand.object;
+          mainStr += "__**Command Info**__";
+          mainStr += "\nHelp: " + command.HELP + "\nUsage: " + command.USAGE;
+          if(command.IS_SUB) {
+            var children = command.CHILDREN;
+            mainStr += "\n**__Sub-Commands__**\n";
+            for(var ii in children) {
+              var child = command.COMMAND + " " + children[ii];
+              var subCommand = registered.find(obj => obj.name == child).object;
+              var requiredGroup = subCommand.REQUIRED_GROUP;
+              var validated = checkUserPerm(guildData, channel, member, requiredGroup);
+              if(validated) {
+                if(!subCommand.IS_SUB) mainStr += "> **> " + subCommand.USAGE + "** - "
+                if(requiredGroup) mainStr += "(" + (requiredGroup.charAt(0).toUpperCase() + requiredGroup.slice(1)) + ") ";
+                if(!subCommand.IS_SUB) mainStr += "*" + subCommand.HELP + "*\n";
+              }
+            }
+            mainStr += "";
           }
-          mainStr += "";
+        } else {
+          channel.send("<@" + member.id + ">, ' " + arg + " ' is not a command.");
+          return;
         }
+        msg.reply("I've sent you a PM with help info.");
+        user.send(mainStr);
+
       } else {
-        mainStr += "' " + arg + " ' is not a command.";
+
+        var mainStr = "";
+
+        mainStr += "__**Available Commands**__\n";
+
+        for(var i in registered) {
+          var command = registered[i].object;
+          var requiredGroup = command.REQUIRED_GROUP;
+          var validated = checkUserPerm(guildData, channel, member, requiredGroup);
+          if(validated) {
+            if(!command.IS_SUB) mainStr += "> **> " + command.USAGE + "** - "
+            if(requiredGroup) mainStr += "(" + (requiredGroup.charAt(0).toUpperCase() + requiredGroup.slice(1)) + ") ";
+            if(!command.IS_SUB) mainStr += "*" + command.HELP + "*\n";
+          }
+        }
+
+        mainStr += "";
+        msg.reply("I've sent you a PM with help info.");
+        user.send(mainStr);
       }
-      msg.reply("I've sent you a PM with help info.");
-      user.send(mainStr);
     }).catch(err => {
       console.log(err);
       channel.send("An internal error has occurred.");
     });
-
-  } else {
-
-    RegisteredCommands.find({}).then(registered => {
-      if(!registered) throw new Error("No data returned!");
-      var mainStr = "";
-
-      mainStr += "__**Available Commands**__\n";
-
-      for(var i in registered) {
-        var command = registered[i].object;
-        var requiredGroup = command.REQUIRED_GROUP;
-        if(!command.IS_SUB) mainStr += "> **> " + command.USAGE + "** - "
-        if(requiredGroup) mainStr += "(" + (requiredGroup.charAt(0).toUpperCase() + requiredGroup.slice(1)) + ") ";
-        if(!command.IS_SUB) mainStr += "*" + command.HELP + "*\n";
-      }
-
-      mainStr += "";
-      msg.reply("I've sent you a PM with help info.");
-      user.send(mainStr);
-    }).catch(err => {
-      console.log(err);
-      channel.send("An internal error has occurred.");
-    });
-  }
+  }).catch(err => {
+    console.log(err);
+    channel.send("An internal error has occurred.");
+  });
 
 }
 
